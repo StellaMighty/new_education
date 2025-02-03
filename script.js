fbd25f0d822e4eb0c3857c5b39711119254b7474
J-max <joelmtn7@gmail.com>
2025-02-03 17:22:26 +0100

    added some animations

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  AOS.init();
+});
+
 const navbarToggler = document.querySelector('.navbar-toggler');
 const navbarCollapse = document.getElementById('navbarNav');
 
